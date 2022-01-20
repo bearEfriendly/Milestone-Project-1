f3f9660cdadfeb1f60ef08b76c36f72dc47711f8
Jonathan Figueroa <sansthehacker@gmail.com>
2022-01-20 00:24:04 -0600

    Finished Loading screen, improved css

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 function LoadingAnim() {
-    console.log("howdy")
     document.getElementById('Pbtn').style.visibility = "hidden"
-    document.getElementById('loading').style.animation = "Loading .5s linear forwards";
-    document.getElementById('color').style.animation = "progress 5s linear forwards";
+    document.getElementById('loading').style.animation = "Loading 1s linear forwards";
+    document.getElementById('color').style.animation = "progress 5s linear";
     document.getElementById('color').style.animationDelay = "1s";
-
+    setTimeout(() => { 
+        document.getElementById('loading').style.visibility = "hidden";
+        document.getElementById('gameBackground').style.visibility = "visible";
+        console.log('howdy')
+        document.getElementById('gameBackground').style.animation = "GameBackground 2s linear forwards";
+        document.getElementById('title-border').style.animation = "BorderWalk 2s linear forwards";
+    }, 6000);
 }
